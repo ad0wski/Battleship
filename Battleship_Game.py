b1f47e683a2f516b180e7874d23a50c80e67c218
Adam <ak.jurkowski@gmail.com>
2025-09-17 11:06:54 +0200

    Dodanie ladnego wypisywania planszy

--- Battleship_Game.py
+++ Battleship_Game.py
@@ -1,1 +1,21 @@
-"Battleship Game"
+plansza = [[" ", " ", " ", " ", " "],
+           [" ", " ", " ", " ", " "],
+           [" ", " ", " ", " ", " "],
+           [" ", " ", " ", " ", " "],
+           [" ", " ", " ", " ", " "]]
+
+def wypisaniePlanszy(plansza):
+        print("\n")
+        print("    1   2   3   4   5 ")
+        print("   ─── ─── ─── ─── ─── ")
+        for i in range(5):
+            for j in range(5):
+                if j == 0:
+                     print(f"{chr(65+i)} | {plansza[i][j]}", end=" |")
+                else:
+                     print(f" {plansza[i][j]}", end=" |")
+            print()
+            print("   ─── ─── ─── ─── ─── ")
+        print("\n")
+
+wypisaniePlanszy(plansza)
