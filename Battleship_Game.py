307b1e2f4067e7c2079b2ceabe048ecb476a88a3
Adam <ak.jurkowski@gmail.com>
2025-09-20 14:29:53 +0200

    Dodanie wielu statkow na plansze

--- Battleship_Game.py
+++ Battleship_Game.py
@@ -6,6 +6,12 @@ plansza = [["  ", "  ", "  ", "  ", "  "],
            ["  ", "  ", "  ", "  ", "  "],
            ["  ", "  ", "  ", "  ", "  "]]
 
+pustaPlansza = [["  ", "  ", "  ", "  ", "  "],
+           ["  ", "  ", "  ", "  ", "  "],
+           ["  ", "  ", "  ", "  ", "  "],
+           ["  ", "  ", "  ", "  ", "  "],
+           ["  ", "  ", "  ", "  ", "  "]]
+
 def wypisaniePlanszy(plansza):
         print("\n")
         print("     1    2    3    4    5 ")
@@ -24,38 +30,64 @@ wypisaniePlanszy(plansza)
 
 orientacjaStatku = ["pionowy", "poziomy"]
 
-def rozmieszczenieStatkow(plansza, orientacjaStatku):
-     orientacja = orientacjaStatku[random.randint(0, 1)]
-     if orientacja == "poziomy":
-          wiersz = random.randint(0, 4)
-          kolumna = random.randint(0, 5 - 3) #(3, to jest dlugosc statku)
-          for i in range(3):
-               plansza[wiersz][kolumna - 1 + i] = "🚢"
-     elif orientacja == "pionowy":
-          wiersz = random.randint(0, 5 - 3)
-          kolumna = random.randint(0, 4)
-          for i in range(3):
-               plansza[wiersz + i][kolumna - 1] = "🚢"
+def rozmieszczenieDanegoStatku(plansza, dlugosc, orientacjaStatku):
+     while True:
+          orientacja = orientacjaStatku[random.randint(0, 1)]
+          if orientacja == "poziomy":
+               wiersz = random.randint(0, 4)
+               kolumna = random.randint(0, 5 - dlugosc)
+               for i in range(dlugosc):
+                    for j in range(dlugosc):
+                         if plansza[wiersz][kolumna + j] != " ":
+                              break
+                    plansza[wiersz][kolumna + i] = "🚢"
+          if orientacja == "pionowy":
+               wiersz = random.randint(0, 5 - dlugosc)
+               kolumna = random.randint(0, 4)
+               for i in range(dlugosc):
+                    for j in range(dlugosc):
+                         if plansza[wiersz][kolumna + j] != " ":
+                              break
+                    plansza[wiersz + i][kolumna] = "🚢"
+               break
 
-rozmieszczenieStatkow(plansza, orientacjaStatku)
+# def rozmieszczenieStatkow(plansza, orientacjaStatku):
+#      orientacja = orientacjaStatku[random.randint(0, 1)]
+#      if orientacja == "poziomy":
+#           wiersz = random.randint(0, 4)
+#           kolumna = random.randint(0, 5 - 3) #(3, to jest dlugosc statku)
+#           for i in range(3):
+#                plansza[wiersz][kolumna + i] = "🚢"
+#      elif orientacja == "pionowy":
+#           wiersz = random.randint(0, 5 - 3)
+#           kolumna = random.randint(0, 4)
+#           for i in range(3):
+#                plansza[wiersz + i][kolumna] = "🚢"
+
+rozmieszczenieDanegoStatku(plansza, 3, orientacjaStatku)
+rozmieszczenieDanegoStatku(plansza, 2, orientacjaStatku)
+rozmieszczenieDanegoStatku(plansza, 1, orientacjaStatku)
 
 def czyTrafione(plansza, wiersz, kolumna):
+     if pustaPlansza[ord(wiersz.upper()) - 65][kolumna - 1] == "💥" or pustaPlansza[ord(wiersz.upper()) - 65][kolumna - 1] == "❌":
+          print("Już strzeliłeś w to miejsce! ")
+          return None
      if plansza[ord(wiersz.upper()) - 65][kolumna - 1] == "🚢":
-          plansza[ord(wiersz.upper()) - 65][kolumna - 1] = "💥"
+          pustaPlansza[ord(wiersz.upper()) - 65][kolumna - 1] = "💥"
           return True
-     plansza[ord(wiersz.upper()) - 65][kolumna - 1] = "❌"
+     pustaPlansza[ord(wiersz.upper()) - 65][kolumna - 1] = "❌"
      return False
 
 trafienia = 0
-while trafienia < 3:
+while trafienia < 6:
      wiersz = input("Podaj wiersz: ")
      kolumna = int(input("Podaj kolumne: "))
      if czyTrafione(plansza, wiersz, kolumna) == True:
           print("Trafiony! ")
           trafienia += 1
-     else:
+     elif czyTrafione(plansza, wiersz, kolumna) == False:
           print("Pudło! ")
-     wypisaniePlanszy(plansza)
+     wypisaniePlanszy(pustaPlansza)
 
 
 print("Wygrałeś! Zatopiłeś wszystkie statki! ")
